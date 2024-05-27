c7024fdba8457233e44e4f109da5832743c2e8b9
Passapol <phasphlsuthahthrrm976@gmail.com>
2024-05-27 16:04:39 +0700

    update 27-5-2567 navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,15 @@ import Link from 'next/link';
 
 const HomePage = () => {
   return (
-    <div className="min-h-[100dvh] flex justify-center items-center">
-      <p>hi สวัสดีครับ</p>
-      <Link href="/plan">จัดตารางเรียน</Link>
-    </div>
+    <>
+      <nav>
+        <img src="/icon-512.png" alt="" />
+      </nav>
+      <div className="min-h-[100dvh] flex justify-center items-center">
+        <p>hi สวัสดีครับ</p>
+        <Link href="/plan">จัดตารางเรียน</Link>
+      </div>
+    </>
   );
 };
 
