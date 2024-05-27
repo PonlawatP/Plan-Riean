0412fc642323b7c3f32239491b18e80d937c8c0d
Passapol <phasphlsuthahthrrm976@gmail.com>
2024-05-27 18:29:16 +0700

    update 27-5-2567 footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,19 @@ import { ReactElement } from 'react';
 import RootLayout from '../app/layout/homelayout';
 import Link from 'next/link';
 import Nav from '../components/hompage/nav_home';
+import Footer from '../components/hompage/footer_home';
 
 
 const HomePage = () => {
   return (
     <>
-       <Nav/>
+      <Nav/>
       <div className="">
         <div className='w-full h-xl bg-Rose'>
 
         </div>
       </div>
+      <Footer/>
     </>
   );
 };
