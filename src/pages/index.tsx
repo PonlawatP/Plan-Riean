7c82eb146f734a03af7a9c121c9fe5119b26a8a4
Phanuwat2828 <131578554+Phanuwat2828@users.noreply.github.com>
2024-05-27 18:33:35 +0700

    layer elemennt

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,33 @@ import Nav from '../components/hompage/nav_home';
 const HomePage = () => {
   return (
     <>
-       <Nav/>
+      <Nav/>
       <div className="">
-        <div className='w-full h-xl bg-Rose'>
+        <section>
+          <div className='flex-col w-full h-full bg-blue-300 align-baseline'>
+              <div className='flex pt-20 w-full justify-center'>
+                <img className='' src="/Logo-Planriean.png" alt=""/>
+                <div className='ml-20 text-[#FFFFFF] flex-col align-middle'>
+                  <h1 className='text-7xl '>PLANRIEAN<br />วางแผน การเรียน</h1>
+                  <h1 className='text-7xl border-b-4 border-[#FFFFFF] mt-2'>ให้ง่ายกว่าที่่เคย</h1>
+                  <div className='text-center'>
+                    <button className='bg-[#FFFFFF] text-black mt-5'>ลองใช้เลย ตอนนี้!</button>
+                  </div>
+                </div>
+              </div>
+              <div className='flex-col justify-center'>
+                  <div className='flex-col w-full text-center'>
+                    <h1 className="text-7xl text-[#FFFFFF]">บริการของเรา</h1>
+                  </div>
+                  <div className="flex-col ">
+                      <h1 className="text-4xl text-[#FFFFFF]">การวางแผนที่สะดวกรวดเร็ว<br />รีวิวรายวิชาจากนักศึกษารายปี<br />เพิ่มความมั่นใจในรายวิชาที่ต้องการเรียน</h1>
+                  </div>
+                      
+                  
+              </div>
+          </div>
+        </section>
 
-        </div>
       </div>
     </>
   );
