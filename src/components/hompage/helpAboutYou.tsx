3abe3ac186ee58c1214e1cf7632bcc7cf87f7297
Passapol <phasphlsuthahthrrm976@gmail.com>
2024-05-28 17:42:31 +0700

    update mt

--- src/components/hompage/helpAboutYou.tsx
+++ src/components/hompage/helpAboutYou.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const nav = () =>{
     return(
         <>
-            <div className="my-[10%]">
+            <div className="mt-[25%]">
                 <div className="text-end text-blue-600 mx-5">
                     <h3 className="max-md:text-2xl text-5xl font-bold">เราช่วยอะไรคุณได้บ้าง</h3>
                     <p className="max-md:text-xs lg:text-2xl">PLAN RIEAN คือตัวช่วยจัดตารางและวางแผนการเรียน ให้ง่ายกว่าที่่เคยเป็น</p> 
