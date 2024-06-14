70d7e34ad05b73fb9bf1d273ef94100a59782b84
PonlawatP <48130528+PonlawatP@users.noreply.github.com>
2024-06-14 14:08:36 +0700

    test: remove certbot

--- gencert.sh
+++
@@ -1,9 +0,0 @@
-docker run -it --rm --name certbot \
-  -v "$(pwd)/data/certbot/conf:/etc/letsencrypt" \
-  -v "$(pwd)/data/certbot/www:/var/www/certbot" \
-  -v "$(pwd)/data/certbot/conf/cloudflare.ini:/etc/letsencrypt/cloudflare.ini" \
-  certbot/certbot certonly --dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/cloudflare.ini \
-  --email plgmcdesign@gmail.com \
-  -d '*.planriean.com' -d 'planriean.com' \
-  --agree-tos \
-  --no-eff-email
