c952618ffb382dc302f31ee79b9c9df11658df66
PonlawatP <48130528+PonlawatP@users.noreply.github.com>
2024-06-14 13:59:32 +0700

    test: certbot again

--- gencert.sh
+++ gencert.sh
@@ -1,8 +1,9 @@
 docker run -it --rm --name certbot \
   -v "$(pwd)/data/certbot/conf:/etc/letsencrypt" \
   -v "$(pwd)/data/certbot/www:/var/www/certbot" \
-  certbot/certbot certonly --webroot -w /var/www/certbot \
+  -v "$(pwd)/data/certbot/conf/cloudflare.ini:/etc/letsencrypt/cloudflare.ini" \
+  certbot/certbot certonly --dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/cloudflare.ini \
   --email plgmcdesign@gmail.com \
-  -d '*.planriean.com' -d planriean.com \
+  -d '*.planriean.com' -d 'planriean.com' \
   --agree-tos \
   --no-eff-email
